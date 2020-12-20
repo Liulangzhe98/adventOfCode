dd69190cb9b3a70dd272eb2b92c211dc3a03eb2d
Thijs van Herwijnen <vanherwijnenthijs98@gmail.com>
2020-12-20 16:12:35 +0100

    Finished Day 19

--- Day 19/messages.py
+++ Day 19/messages.py
@@ -3,7 +3,7 @@ import re
 def main():
     #415 -> a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b
     
-    with open("test2.txt", "r") as openobj:
+    with open("input.txt", "r") as openobj:
         rules = []
         messages = []
         for line in openobj:
@@ -21,11 +21,23 @@ def main():
     print(f"Headrule : {headrule}")
         
     while re.search("\d", headrule):
-    #for _ in range(15):
+    #for _ in range(2):
         newRule = ""
         for toReplace in headrule.split(" "):
             if toReplace.isdigit():
                 replacement = rules[toReplace]
+                #if toReplace in replacement: # special inf loop protection
+                    #a = [x for x in replacement.split(" | ") if toReplace not in x]
+                    #if toReplace == '8':
+                        #newRule += " ( "  + "".join(a) + " )+ "
+                    #else:
+                        #rule_11 = ""
+                        #for x in range(1,5):
+                            #options = "".join(a).split(" ")
+                            #rule_11 += f"( ({options[0]} ){{{x}}}({options[1]} ){{{x}}} )"
+                        #print(rule_11)
+                    
+                #else:
                 if "|" in replacement:
                     newRule += " ( "+replacement+" ) "
                 else:
@@ -33,6 +45,7 @@ def main():
             else:
                 newRule += " " + toReplace
         headrule = newRule.strip()
+        print(f"Head: {headrule}")
     headrule = "^"+"".join(headrule.split(" "))
     headrule += "$"
     print(f"Final: {headrule}")
