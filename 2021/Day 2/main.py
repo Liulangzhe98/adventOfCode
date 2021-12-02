274d9f79e3c58355ca847db71447a1923bf02441
Thijs <vanherwijnenthijs98@gmail.com>
2021-12-02 11:20:31 +0100

    Clean up + oneliners - Day 2 [2021]

--- 2021/Day 2/main.py
+++ 2021/Day 2/main.py
@@ -1,13 +1,23 @@
+def sign(x):
+    return -1 if x < 0 else 1
+
 def part_one(input_list):
     pos = depth = 0
     for d, v in input_list:
         if d == "up":
-            depth -= int(v)
+            depth -= v
         elif d == "down":
-            depth += int(v)
+            depth += v
         else:
-            pos += int(v)
-    print("What do you get if you multiply your final horizontal position by your final depth?")
+            pos += v
+    print("(1) What do you get if you multiply your final horizontal position by your final depth?")
+    print(f"Answer: pos: {pos} depth: {depth} -> {pos*depth}")
+
+def part_one_one_liner(input_list):
+    pos = depth = 0
+    [(pos := pos + v) if d.startswith("f") else (0, depth := depth - v*sign(ord(d[0])-ord('e'))) for d, v in input_list]
+
+    print("(1) What do you get if you multiply your final horizontal position by your final depth?")
     print(f"Answer: pos: {pos} depth: {depth} -> {pos*depth}")
 
 
@@ -15,19 +25,30 @@ def part_two(input_list):
     pos = depth = aim = 0
     for d, v in input_list:
         if d == "up":
-            aim -= int(v)
+            aim -= v
         elif d == "down":
-            aim += int(v)
+            aim += v
         else:
-            pos += int(v)
-            depth += aim*int(v)
-    print("What do you get if you multiply your final horizontal position by your final depth?")
+            pos += v
+            depth += aim*v
+    print("(2) What do you get if you multiply your final horizontal position by your final depth?")
     print(f"Answer: pos: {pos} depth: {depth} -> {pos*depth}")
+
+def part_two_one_liner(input_list):
+    pos = depth = aim = 0
+    [(pos := pos + v, depth:= depth+aim*v) if d.startswith("f") else (aim:= aim + (v*-1*sign(ord(d[0])-ord('e')))) for d, v in input_list]
+    print("(2) What do you get if you multiply your final horizontal position by your final depth?")
+    print(f"Answer: pos: {pos} depth: {depth} -> {pos*depth}")
+    
 
 def main():
     with open("input.txt") as file:
         input_list = [x.strip().split(" ") for x in file.readlines()]
+        input_list = [(d, int(v)) for d, v in input_list]
     part_one(input_list)
     part_two(input_list)
+    print(" === one liners === ")
+    part_one_one_liner(input_list)
+    part_two_one_liner(input_list)
 
 main()
