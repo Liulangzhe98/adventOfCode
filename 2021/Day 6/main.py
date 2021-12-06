24285637c6d36cd16a33a44f5b1afc9556900bfc
Thijs <vanherwijnenthijs98@gmail.com>
2021-12-06 11:39:48 +0100

    Clean up + personal challenger - Day 6 [2021]

--- 2021/Day 6/main.py
+++ 2021/Day 6/main.py
@@ -1,45 +1,21 @@
-import re
-
-def part_one(input_list, my_dict):
-    MAX_DEPTH = 80
-    rec_dict = {}
-    for x in range(1, 8):
-        rec_dict[x-1] = recursion(x, MAX_DEPTH,my_dict)
-    x = [my_dict.get(y+1) for y in input_list]
-    print("(1) How many lanternfish would there be after 80 days?")
-    print(f"Answer: {sum(x)}")
-
-
-def part_two(input_list, my_dict):
-    MAX_DEPTH = 256
-    rec_dict = {}
-    for x in range(1, 8):
-        rec_dict[x-1] = recursion(x, MAX_DEPTH,my_dict)
-    x = [my_dict.get(y+1) for y in input_list]
-    print("(2) How many lanternfish would there be after 256 days?")
-    print(f"Answer: {sum(x)}")
-
+def solver(input_list, my_dict, MAX_DEPTH=80):
+    [recursion(x, MAX_DEPTH,my_dict) for x in range(1, 8)]
+    return sum([my_dict.get(y+1) for y in input_list])
     
-def init():
-    with open("input.txt") as file:
-        input_list = [int(x) for x in file.readline().split(",")]
-    return input_list
-
 def recursion(depth, MAX, my_dict):
     if my_dict.get(depth):
         return my_dict.get(depth)
-
     if depth > MAX:
         return 1
     output = recursion(depth+7, MAX,my_dict)+recursion(depth+9, MAX,my_dict)
     my_dict[depth] = output
     return output
 
-
 def main():
-    init_vars = init()
-    part_one(sorted(init_vars), {})
-    part_two(sorted(init_vars), {}) 
- 
+    init_vars = [int(x) for x in open("input.txt").readline().split(",")]
+    print("(1) How many lanternfish would there be after 80 days?")
+    print(f"Answer: {solver(init_vars, {}, 80)}")
+    print("(2) How many lanternfish would there be after 256 days?")
+    print(f"Answer: {solver(init_vars, {}, 256)}")
 
 main()
