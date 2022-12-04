67909be3154baddcc863d1ea3ee39e74a366db9f
Thijs <vanherwijnenthijs98@gmail.com>
2022-12-04 11:44:50 +0100

    Created template and script for automating folder creation

---
+++ 2022/create_aoc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+day=${1?Missing day modifier.}
+
+mkdir "Day $day"
+cp template.py "Day $day/main.py"
+touch "Day $day/test.txt" "Day $day/input.txt"
+echo "Succesfully created the folder for Day $day"
---
+++ 2022/template.py
@@ -0,0 +1,21 @@
+def part_one(file_path):
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            continue
+    return None
+
+
+def part_two(file_path):
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            continue
+    return None
+
+def main():
+    print(f"Solution 1T: {part_one('test.txt')}")
+    print(f"Solution 2T: {part_two('test.txt')}")
+    print(f"Solution 1 : {part_one('input.txt')}")
+    print(f"Solution 2 : {part_two('input.txt')}")
+    
+if __name__ == "__main__":
+    main()
