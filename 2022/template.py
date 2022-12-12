9be909893f27da53250c8324672e07473c198ef5
Thijs <vanherwijnenthijs98@gmail.com>
2022-12-12 11:40:25 +0100

    Added timed printing to template

--- 2022/template.py
+++ 2022/template.py
@@ -7,15 +7,23 @@ def part_one(file_path):
 
 def part_two(file_path):
     with open(file_path, 'r') as file:
-        ffor line in file.read().splitlines():
+        for line in file.read().splitlines():
             continue
     return None
 
+def timed_print(text, func, file):
+    import time
+    start = time.time()
+    result = func(file)
+    end = time.time()
+    print(f"{text} took {(end-start)*1000:>8.2f}ms : {result} ")
+
+
 def main():
-    print(f"Solution 1T: {part_one('test.txt')}")
-    print(f"Solution 2T: {part_two('test.txt')}")
-    print(f"Solution 1 : {part_one('input.txt')}")
-    print(f"Solution 2 : {part_two('input.txt')}")
+    timed_print("Solution 1T", part_one, "test.txt")
+    timed_print("Solution 2T", part_two, "test.txt")
+    timed_print("Solution 1 ", part_one, "input.txt")
+    timed_print("Solution 2 ", part_two, "input.txt")
     
 if __name__ == "__main__":
     main()
