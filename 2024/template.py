d955f9e5c09c55c40596fc90a1b9362fed81f24a
Thijs van Herwijnen <vanherwijnenthijs98@gmail.com>
2024-12-03 08:11:36 +0100

    Set up the project for this year

---
+++ 2024/template.py
@@ -0,0 +1,31 @@
+def part_one(file_path):
+    with open(file_path, 'r') as file:
+        for line in file.read().strip().splitlines():
+            continue
+    return None
+
+
+def part_two(file_path):
+    with open(file_path, 'r') as file:
+        for line in file.read().strip().splitlines():
+            continue
+    return None
+
+
+def timed_print(text, func, file):
+    import time
+    start = time.time()
+    result = func(file)
+    end = time.time()
+    print(f"\033[96m{text} took {(end-start)*1000:>8.2f}ms : {result} \033[0m")
+
+
+def main():
+    timed_print("Solution 1T", part_one, "test.txt")
+    timed_print("Solution 2T", part_two, "test.txt")
+    timed_print("Solution 1 ", part_one, "input.txt")
+    timed_print("Solution 2 ", part_two, "input.txt")
+    
+
+if __name__ == "__main__":
+    main()
