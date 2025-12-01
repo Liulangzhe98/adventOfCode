0a9298ef711f606aa9f06a1fe9b70bf69368f080
Thijs <vanherwijnenthijs98@gmail.com>
2025-12-01 08:22:35 +0100

    A new year, a new run at all the stars

---
+++ 2025/Day 01/main.py
@@ -0,0 +1,59 @@
+def part_one(file_path):
+    dial = 50
+    count = 0
+    with open(file_path, 'r') as file:
+        for line in file.read().strip().splitlines():
+            a = list(line) 
+            direction = a[0]
+            amount = int("".join(a[1:]))
+            if direction == "R":
+                dial = (dial + amount) % 100
+            else:
+                dial = (dial - amount) % 100
+            if dial == 0:
+                count += 1
+    return count
+
+
+def part_two(file_path):
+    dial = 50
+    count = 0
+    with open(file_path, 'r') as file:
+        for line in file.read().strip().splitlines():
+            a = list(line) 
+            direction = a[0]
+            amount = int("".join(a[1:]))
+            
+
+            if direction == "R":
+                if dial + amount > 100:
+                    count += (dial + amount) // 100
+                dial = (dial + amount) % 100
+            else:
+                if dial - amount < 0 and dial != 0:
+                    print("here", dial-amount, (dial - amount)//100)
+                    count += abs((dial - amount) // 100) 
+                dial = (dial - amount) % 100
+            if dial == 0:
+                count += 1
+            print(direction, amount, dial, count)
+    return count
+
+
+def timed_print(text, func, file):
+    import time
+    start = time.time()
+    result = func(file)
+    end = time.time()
+    print(f"\033[96m{text} took {(end-start)*1000:>8.2f}ms : {result} \033[0m")
+
+
+def main():
+    timed_print("Solution 1T", part_one, "test.txt")
+    timed_print("Solution 2T", part_two, "test.txt")
+    timed_print("Solution 1 ", part_one, "input.txt")
+    timed_print("Solution 2 ", part_two, "input.txt")
+    
+
+if __name__ == "__main__":
+    main()
